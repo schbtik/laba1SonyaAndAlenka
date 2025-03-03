79d5fca6d252bd05c56c96f4702722cba6cd6166
OlenaSkakun-IPZ-22 <olena_skakunn@knu.ua>
2025-03-03 15:20:37 +0200

    апдейт для класу лабіринт(додала 2 дод. методи)

--- maze.py
+++ maze.py
@@ -28,4 +28,17 @@ class Maze:
  
     def is_power_pellet(self, x, y): 
         """Перевіряє, чи є енергоджайзер у комірці""" 
-        return self.grid[y][x] == 'O'
+        return self.grid[y][x] == 'O' 
+ 
+    def eat_point(self, x, y): 
+        """З'їдає точку або енергоджайзер""" 
+        if self.grid[y][x] in ['.', 'O']: 
+            self.grid[y][x] = ' '  # Очистка клітинки 
+            return True 
+        return False 
+ 
+    def update_position(self, old_x, old_y, new_x, new_y, entity): 
+        """Оновлює розташування сутності (Pac-Man або привида) у лабіринті""" 
+        self.grid[old_y][old_x] = " "  # Очищаємо стару позицію 
+        self.grid[new_y][new_x] = entity  # Встановлюємо нову 
+        return 0
